f4e0a1f565a14266e4a6721b35bcffbf693fb4d3
haitran1921 <54724402+haitran1921@users.noreply.github.com>
2025-03-19 13:38:31 +0700

    Add next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="next" />
+/// <reference types="next/navigation-types/compat/navigation" />
+/// <reference types="next/image-types/global" />
+/// <reference types="next/app-types" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
